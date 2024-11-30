fda4020a0d2a4439b6a5aede2f1e68b3831e2eb2
holy-jesus <hj@holy-coder.ru>
2024-11-30 11:47:35 +0500

    Почистил файл от тестового кода; Переназвал функцию

--- checker/checker.py
+++ checker/checker.py
@@ -1,27 +1,9 @@
-import asyncio
-
 from playwright.async_api import async_playwright
-
-
-# async def main():
-#     async with async_playwright() as p:
-#         browser = await p.firefox.launch(headless=False)
-#         page = await browser.new_page()
-#         await page.goto("https://playwright.dev/python/docs/library")
-#         print(await page.title())
-#         await asyncio.sleep(10)
-#         await browser.close()
 
 
 class Checker:
     def __init__(self) -> None:
         self.manager = async_playwright()
 
-    async def get_available_checks(self):
+    async def get_available_tests(self):
         pass
-
-
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
